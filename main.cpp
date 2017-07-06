3d9042ef494ae7b5ace21b792bcd62a9c20949a3
Masataka Mori <masataka.mori20@gmail.com>
2017-07-06 16:10:52 +0900

    add basic functions

--- main.cpp
+++ main.cpp
@@ -2,9 +2,47 @@
 
 using namespace std;
 
+int evenNumber(int x);// function for even number
+int oddNumber(int x);// function for odd number
+
 int main() 
 {
-	cout << "Hello World!!" << endl;
+	int n; // Input number;
+
+	n = 22;
+	do
+	{
+		if(n % 2)
+		{
+			// n is odd
+			n = oddNumber(n);
+		}
+		else 
+		{
+			// n is even
+			n = evenNumber(n);
+		}
+
+		cout << n << ' ';
+	} while(n != 1);
 
 	return 0;
+}
+
+
+int oddNumber(int x)
+{
+	int n = 0;
+	n = 3*x + 1;
+
+	return n;
+}
+
+
+int evenNumber(int x)
+{
+	int n = 0;
+	n = x / 2;
+
+	return n;
 }
