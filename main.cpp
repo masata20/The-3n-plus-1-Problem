ec6c6b1b30dd37265d3e570baf2ad7713de8ca26
Masataka Mori <masataka.mori20@gmail.com>
2017-07-06 16:52:50 +0900

    added counter function for cycle length

--- main.cpp
+++ main.cpp
@@ -2,34 +2,56 @@
 
 using namespace std;
 
-int evenNumber(int x);// function for even number
-int oddNumber(int x);// function for odd number
+
+int cycleLength(int);// count cycle lengh
+int evenNumber(int);// function for even number
+int oddNumber(int);// function for odd number
 
 int main() 
 {
-	int n; // Input number;
+	int i = 900, j = 1000;// Input numbers
 
-	n = 22;
+	int n = 0, max_cycleLength = 0; // Counter for cycle length
+
+	for(i; i <= j; i++)
+	{
+
+		n = cycleLength(i);
+		if (n > max_cycleLength)
+			max_cycleLength = n;
+	}
+	
+	cout << i << ' ' << j << ' ' << max_cycleLength << endl;
+	return 0;
+}
+
+// count cycle length for given number with arugument
+// and return the cycle-length for int x
+int cycleLength(int x)
+{
+	int counter = 1;
 	do
 	{
-		if(n % 2)
+		if(x % 2)
 		{
 			// n is odd
-			n = oddNumber(n);
+			x = oddNumber(x);
 		}
 		else 
 		{
 			// n is even
-			n = evenNumber(n);
+			x = evenNumber(x);
 		}
 
-		cout << n << ' ';
-	} while(n != 1);
+		counter++;	
 
-	return 0;
+	} while(x != 1);
+
+	return counter;
 }
 
-
+// for odd numbere, multiply by 3 and plus 1
+// and return that number
 int oddNumber(int x)
 {
 	int n = 0;
@@ -38,7 +60,8 @@ int oddNumber(int x)
 	return n;
 }
 
-
+// for even number, divided by 2
+// and return it
 int evenNumber(int x)
 {
 	int n = 0;
